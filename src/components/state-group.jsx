59e523b5d29333db64d27c784346bca4387d3e81
Rehan Ahmad <rehan.ahmad@dazn.com>
2021-02-18 14:32:19 +0000

    prevent default

--- src/components/state-group.jsx
+++ src/components/state-group.jsx
@@ -4,14 +4,16 @@ import { Segmented, Button } from 'framework7-react';
 import '../css/state-group.css';
 
 export default function StateGroup({ labels, onChange, value, slot, header, isDirty }) {
-  const classes = ['state-group', isDirty ? 'dirty' : '', header && 'header'].join(' ');
+  const classes = ['state-group', isDirty ? 'dirty' : '', header && 'header'].join(' ').trim();
   return (
     <Segmented raised className={classes} slot={slot}>
       {labels.split('').map((label, i) => (
         <Button
           key={i}
           active={(value !== undefined && value.toString() === label) || header}
-          onClick={() => {
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
             const onchangeParam =
               value !== undefined && value.toString() === label ? undefined : label;
             onChange(onchangeParam);
